c408b82511f7a15ee206c81dc56a15ba4ca7de66
Alex Chiling <aleksandraspenova@gmail.com>
2023-11-22 15:46:47 +0300

    +закончили

--- 1 part.py
+++ 1 part.py
@@ -2,47 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from tkinter import *
 
-class Window:
-   def __init__(self, links):
-      self.root = Tk()
-      self.root.geometry("800x600")
-      self.root.title("Прогноз погоды")
-      self.check = []
-
-      #Настройки текста
-      self.label = Label(self.root, wraplength=800, font=("Trebuchet MS", 11))
-      self.label.grid(row=0, column=0, columnspan=2)
-      self.set_text(links)
-
-      #Поле ввода
-      self.entry = Entry(self.root, width=50)
-      self.entry.grid(row=1, column=0, sticky='E')
-
-      #Создание кнопки
-      self.btn = Button(self.root, text="Ввод", command=lambda x=links: self.check_input(links))
-      self.btn.grid(row=1, column=1, sticky='W')
-
-   '''Создадим функцию установки текста на экране. 
-   Она будет трансформировать словарь links в строку.'''
-   def set_text(self, links):
-      text = ""
-      for city in links:
-         self.check.append(city)
-         text += city + ', '
-      text = text[:-2]
-      self.label.configure(text=text)
-
-   '''Добавим функцию проверки 
-   введённых пользователем значений.'''
-
-   def check_input(self, links):
-      choice = self.entry.get()
-      if choice not in self.check:
-         return
-      #self.parse_weather(links[choice])
-
-
-
 class Weather:
    def __init__(self,link):
       self.link = link
@@ -72,7 +31,56 @@ class Weather:
 
       return Window(links).root.mainloop()
 
+class Window:
+   def __init__(self, links):
+      self.root = Tk()
+      self.root.geometry("800x600")
+      self.root.title("Прогноз погоды")
+      self.check = []
 
+      #Настройки текста
+      self.label = Label(self.root, wraplength=800, font=("Trebuchet MS", 11))
+      self.label.grid(row=0, column=0, columnspan=2)
+      self.set_text(links)
+
+      #Поле ввода
+      self.entry = Entry(self.root, width=50)
+      self.entry.grid(row=1, column=0, sticky='E')
+
+      #Создание кнопки
+      self.btn = Button(self.root, text="Ввод", command=lambda x=links: self.check_input(links))
+      self.btn.grid(row=1, column=1, sticky='W')
+
+
+   '''Создадим функцию установки текста на экране. 
+   Она будет трансформировать словарь links в строку.'''
+   def set_text(self, links):
+      text = ""
+      for city in links:
+         self.check.append(city)
+         text += city + ', '
+      text = text[:-2]
+      self.label.configure(text=text)
+
+   '''Добавим функцию проверки 
+   введённых пользователем значений.'''
+
+   def check_input(self, links):
+      choice = self.entry.get()
+      if choice not in self.check:
+         return
+      self.parse_weather(links[choice])
+      #self.parse_weather(links[choice])
+
+   def parse_weather(self, link):
+      w = Weather(link)
+      data = w.soup.find('div', {'id': 'archiveString'})
+      temp = data.find('span', {'class': 't_0'}).text
+      text = data.find('a', {'class': 'ArchiveStrLink'})
+      if text is None:
+         text = data.find('div', {'class': 'ArchiveInfo'})
+      text = text.text.replace("Архив погоды на метеостанции", "")
+      self.label.configure(text=temp + "\n" + text)
 
 w = Weather("https://rp5.ru/Погода_в_России")
 w.get_cities()
