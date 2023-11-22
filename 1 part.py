b41d08857b641c5afd71597cec3f231e7cf8e2f1
Alex Chiling <aleksandraspenova@gmail.com>
2023-11-22 11:06:50 +0300

    + Передача ссылки вне класса + Фильтрация ссылок

--- 1 part.py
+++ 1 part.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 
 class Weather:
-   def __init__(self):
-      self.link = f"https://rp5.ru/Погода_в_России"
+   def __init__(self,link):
+      self.link = link
 
       r = requests.get(self.link).text
       self.soup = BeautifulSoup(r, "html.parser")
@@ -21,12 +21,15 @@ class Weather:
          text = text[text.find('href="'):][6:]
          last = text.find('"')
          text = text[:last]
-         links[name] = "https://rp5.ru" + text
-         print(block)
+
+         if name not in ('Мобильная версия', 'Главная', 'О сайте', 'Частые вопросы (FAQ)', 'Контакты',
+                         'Литва', 'Беларусь', 'Россия', 'Украина', 'Все страны', '>>>', 'См. на карте', ''):
+            links[name] = "https://rp5.ru" + text
+            print(links[name])
 
       print(links)
 
       return links
 
-w = Weather()
+w = Weather("https://rp5.ru/Погода_в_России")
 w.get_cities()
